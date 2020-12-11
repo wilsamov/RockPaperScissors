74678e391c9f948c0eb4c63968782c43f2409f78
wilsamov <wilsamov@gmail.com>
2020-12-11 10:37:13 -0800

    Started work on new UI. Still Incomplete

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let winner;
 let playerScore = 0;
 let computerScore = 0;
 let roundNumber = 1;
+const box = document.getElementById('box');
+const results = document.getElementById('results');
 
 //function that randomly chooses one of the options
 function computerPlay() {
@@ -17,84 +19,63 @@ function computerPlay() {
 
 //prompts player to choose
 function playerPlay() {
-  let choice = prompt(`Round ${roundNumber}. Rock, paper, or scissors?`);
-  choice = choice.toLowerCase();
-  choice = choice.trim();
-  if (choice == "rock" || choice == "paper" || choice == "scissors") {
-    playerSelection = choice;
-    alert(`You chose ${playerSelection}.`);
+  const rock = document.getElementById('rock');
+  const paper = document.getElementById('paper');
+  const scissors = document.getElementById('scissors');
+  const content = document.createElement('h3');
+
+  content.style.cssText = "font-family: arial; color: #990099";
+
+  rock.addEventListener('click', (e) => {
+    playerSelection = moveOptions[0];
+    console.log(playerSelection);
+    content.textContent = `You choose ${playerSelection}.`;
+    box.appendChild(content);
     return playerSelection;
-  } else if (choice !== "rock" || choice !== "paper" || choice!== "scissors") {
-    alert("Sorry, input invalid, please type either rock, paper or scissors.");
-  } else {
-    alert("Sorry, input invalid, please type either rock, paper or scissors.");
-  }
+  });  
+
+  paper.addEventListener('click', (e) => {
+    playerSelection = moveOptions[1];
+    console.log(playerSelection);
+    content.textContent = `You choose ${playerSelection}.`;
+    box.appendChild(content);
+    return playerSelection;
+  }); 
+
+  scissors.addEventListener('click', (e) => {
+    playerSelection = moveOptions[2];
+    console.log(playerSelection);
+    content.textContent = `You choose ${playerSelection}.`;
+    box.appendChild(content);
+    return playerSelection;
+  });
 }
 
-//Function to play a single round
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == "rock"){
-    if (computerSelection == "scissors") {
-      alert("You win, rock beats scissors!");
+  const play = document.getElementById('play');
+  const content2 = document.createElement('h3');
+
+  play.addEventListener('click', (e) => {
+    if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper") {
+      winner = "Player";
       playerScore++;
-    } else if (computerSelection == "rock") {
-      alert("You tied! Computer also chose rock.");
-    } else if (computerSelection == "paper") {
-      alert("You lose, paper beats rock.");
+      content2.textContent = `${winner} wins. You have ${playerScore} points. Computer has ${computerScore} points. Round ${roundNumber} over.`;
+      results.appendChild(content2);
+      roundNumber++;
+    } else if (playerSelection === "scissors" && computerSelection === "rock" || playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
+      winner = "Computer";
       computerScore++;
+      content2.textContent = `${winner} wins. You have ${playerScore} points. Computer has ${computerScore} points. Round ${roundNumber} over.`;
+      results.appendChild(content2);
+      roundNumber++;
     } else {
-      alert("Please try again.");
+      content2.textContent = `You tied. Round ${roundNumber} over.`;
+      results.appendChild(content2);
+      roundNumber++;
     }
-  } else if (playerSelection == "scissors") {
-    if (computerSelection == "paper") {
-      alert("You win, scissors beat paper!");
-      playerScore++;
-    } else if (computerSelection == "scissors") {
-      alert("You tied! Computer also chose scissors.");
-    } else if (computerSelection == "rock") {
-      alert("You lose, rock beats scissors.");
-      computerScore++;
-    } else {
-      alert("Please try again.");
-    }
-  } else if (playerSelection == "paper") {
-    if (computerSelection == "rock") {
-      alert("You win, paper beats rock!");
-      playerScore++;
-    } else if (computerSelection == "paper") {
-      alert("You tied! Computer also chose paper.");
-    } else if (computerSelection == "scissors") {
-      alert("You lose, scissors cut paper.");
-      computerScore++;
-    } else {
-      alert("Please try again.");
-    } 
-  } else {
-    alert("Please try again");
-  }
+  });
 }
 
-//function that declares winner
-function winnerScore(computerScore, playerScore) {
-  if (computerScore > playerScore) {
-      alert(`Computer won ${computerScore} out of 5 rounds.`);
-      winner = "computer";
-    } else if (playerScore > computerScore) {
-      alert(`Player won ${playerScore} out of 5 rounds.`);
-      winner = "player";
-    } else {
-      alert(`You tied with computer!`);
-    }
-}
-
-//repeats round 5 times
-function game() {
-  for (i=0; i < 5; i++) {
-    computerPlay();
-    playerPlay();
-    playRound(playerSelection, computerSelection);
-    roundNumber++;
-  } winnerScore(computerScore, playerScore);
-}
-
-game();
+computerPlay();
+playerPlay();
+playRound(playerSelection, computerSelection);
